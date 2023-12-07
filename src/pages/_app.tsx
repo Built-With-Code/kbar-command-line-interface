f26e25d484ef02c30a49bfaf39ee65bd82b00c8d
Akhil Waghmare <akhilwaghmare@gmail.com>
2023-12-07 11:10:54 -0800

    Completed project

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,53 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import { useState } from "react";
+import CommandBar from "@/components/CommandBar";
+import { useRouter } from "next/navigation";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const [activeTheme, setActiveTheme] = useState("light");
+  const router = useRouter();
+
+  const actions = [
+    {
+      id: "home",
+      name: "Home",
+      section: "navigation",
+      shortcut: ["h"],
+      keywords: "home",
+      perform: () => router.push("/"),
+    },
+    {
+      id: "about",
+      name: "About",
+      section: "navigation",
+      shortcut: ["a"],
+      keywords: "about, contact",
+      perform: () => router.push("/about"),
+    },
+    {
+      id: "dark-theme",
+      name: "Dark theme",
+      section: "utilities",
+      shortcut: ["d"],
+      keywords: "dark, theme, mode",
+      perform: () => setActiveTheme("dark"),
+    },
+    {
+      id: "light-theme",
+      name: "Light theme",
+      section: "utilities",
+      shortcut: ["l"],
+      keywords: "light, theme, mode",
+      perform: () => setActiveTheme("light"),
+    },
+  ];
+
+  return (
+    <CommandBar actions={actions}>
+      <div className={`${activeTheme}`}>
+        <Component {...pageProps} />
+      </div>
+    </CommandBar>
+  );
 }
---
+++ src/pages/about.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-center p-24 dark:bg-neutral-800">
+      <h1 className="text-4xl font-semibold dark:text-white">About</h1>
+    </main>
+  );
+};
+
+export default About;
